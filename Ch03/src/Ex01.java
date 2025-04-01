234c653b326b019c8cbcd432b55f8d8297639d15
chador <newspark00@gmail.com>
2025-04-01 16:51:53 +0900

    ch3(*), ch5(배열)과제

---
+++ Ch03/src/Ex01.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Ex01 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++) {
+            System.out.println('*');
+            for(int j=1;j<=i;j++){
+                System.out.print(' ');
+            }
+        }
+    }
+}
